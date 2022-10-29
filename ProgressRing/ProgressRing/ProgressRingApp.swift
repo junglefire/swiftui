8e49cdcdd1644202b9352de9347734a5ab3e4b5a
alex.wz <alex.wz@@alibaba-inc.com>
2022-10-29 11:32:44 +0800

    done

---
+++ ProgressRing/ProgressRing/ProgressRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressRingApp.swift
+//  ProgressRing
+//
+//  Created by alex on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ProgressRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProgressRing/ProgressRing/RingTip.swift
@@ -0,0 +1,42 @@
+//
+//  RingTip.swift
+//  ProgressRing
+//
+//  Created by alex on 2022/10/29.
+//
+
+import Foundation
+import SwiftUI
+
+struct RingTip: Shape {
+    var progress: Double = 0.0
+    var startAngle: Double = -90.0
+    var ringRadius: Double
+    
+    private var position: CGPoint {
+        let angle = 360 * progress + startAngle
+        let angleInRadian = angle * .pi / 180
+        
+        return CGPoint(x: ringRadius * cos(angleInRadian), y: ringRadius * sin(angleInRadian))
+    }
+    
+    var animatableData: Double {
+        get { progress }
+        set { progress = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        guard progress > 0.0 else {
+            return path
+        }
+                
+        let frame = CGRect(x: position.x, y: position.y, width: rect.size.width, height: rect.size.height)
+        
+        path.addRoundedRect(in: frame, cornerSize: frame.size)
+        
+        return path
+    }
+
+}
