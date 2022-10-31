3b1af6751e237329ae657cbd782068544f88e354
alex.wz <alex.wz@@alibaba-inc.com>
2022-10-31 09:40:37 +0800

    done

---
+++ TabView/TabView/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  TabView
+//
+//  Created by alex on 2022/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            TabView(selection: $selection) {
+                Text("Home Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+                    .tag(0)
+
+                Text("Bookmark Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("Bookmark")
+                    }
+                    .tag(1)
+
+                Text("Video Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "video.circle.fill")
+                        Text("Video")
+                    }
+                    .tag(2)
+
+                Text("Profile Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("Profile")
+                    }
+                    .tag(3)
+            }
+            .accentColor(.red)
+            .onAppear() {
+                UITabBar.appearance().barTintColor = .green
+            }
+            
+            Button(
+                action: {
+                    selection = (selection + 1) % 4
+                }
+            ) {
+                Text("Next")
+                    .font(.system(.headline, design: .rounded))
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(10.0)
+                    .padding()
+            }
+        }
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ TabView/TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by alex on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
